b9e58310ccfc758b0422a2aa5941c0be7de6cc50
xujiawei <2225927212@qq.com>
2024-11-28 14:26:36 +0800

    简单的 netlify 测试

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { formatDistanceToNow } from "date-fns";
 
-const date = "1996-09-13 10:00:00";
+const date = "2005-01-15 10:00:00";
 document.body.textContent = `${formatDistanceToNow(new Date(date))} ago`;
